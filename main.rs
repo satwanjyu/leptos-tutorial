929884563aa83b6eef54b694b5afb0331e121037
satwanjyu <satwanjyu@outlook.com>
2024-04-28 15:13:53 +0800

    3.1 A Basic Component

--- main.rs
+++ main.rs
@@ -3,6 +3,22 @@ use leptos::*;
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(|| {
-        view! { <p>"Hello , world!"</p> }
+        view! { <App/> }
     })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+        >
+            "Click me: "
+            {count}
+        </button>
+    }
 }
