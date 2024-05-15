e830ec1811dd9b3128d2a34be84db6d139ca874b
satwanjyu <satwanjyu@outlook.com>
2024-05-15 09:04:22 +0000

    3.3 components

--- main.rs
+++ main.rs
@@ -13,21 +13,26 @@ fn App() -> impl IntoView {
     let double_count = move || count() * 2;
 
     view! {
-        <button
-            on:click=move |_| {
-                set_count.update(|n| *n += 1);
-            }
-        >
-            "Click me: "
-            {count}
+        <button on:click=move |_| {
+            set_count.update(|n| *n += 1);
+        }>
+
+            "Click me: " {count}
         </button>
-        <progress
-            max="50"
-            value=double_count
-        />
-        <p>
-            "Double Count: "
-            {double_count}
-        </p>
+        <ProgressBar progress=count/>
+        <ProgressBar progress=Signal::derive(double_count)/>
     }
+}
+
+/// Shows progress toward a goal.
+#[component]
+fn ProgressBar(
+    /// The maximum value of the progress bar.
+    #[prop(default = 100)]
+    max: u16,
+    /// How much progress should be displayed.
+    #[prop(into)]
+    progress: Signal<i32>,
+) -> impl IntoView {
+    view! { <progress max="50" value=progress></progress> }
 }
